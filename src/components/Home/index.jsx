639f2dd306ce85a11d416387eb76143d24f02438
Permanaa <irmanpermana.lu@gmail.com>
2020-10-09 21:04:23 +0700

    initial commit

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="container">
+        <h1 className="text-center">Home</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
