3b8d925e3d58d66b229c4e216318e45ec82cc9aa
Permanaa <irmanpermana.lu@gmail.com>
2020-10-10 11:57:20 +0700

    add repository route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from './components/Home'
+import Repo from './components/Repo'
 
 function App() {
   return (
     <BrowserRouter>
         <Route path='/' exact component={Home} />
+        <Route path='/repository/:username' exact component={Repo} />
     </BrowserRouter>
   );
 }
